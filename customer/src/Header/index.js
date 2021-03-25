d4a5a59993e720c570e07b9b0a56c93a0589c5c9
VadimSido <vadsid74@gmail.com>
2021-03-25 20:42:36 +0200

    fix: Creating component and style Header

---
+++ customer/src/Header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+    Link
+} from "react-router-dom";
+import styles from "./Header.module.css";
+
+const Header = () => {
+    return(
+        <div className={styles.container}>
+            <p><Link to="/statistics" className={styles.link}><strong>STATISTICS</strong></Link> </p>
+            <p><Link to="/QR-code" className={`${styles.link} ${styles.active}`}><strong>QR-CODE</strong></Link> </p>
+            <p><Link to="profile" className={styles.link}><strong>PROFILE</strong></Link> </p>
+        </div>
+    )
+}
+
+export default Header;
