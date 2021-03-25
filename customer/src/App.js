9fb0d4b41e1f398d8749f6d6b8cbe543955248c9
VadimSido <vadsid74@gmail.com>
2021-03-25 20:12:38 +0200

    fix: deleting old code App added new conponent to App

--- customer/src/App.js
+++ customer/src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Header from "./Header";
+import Feedbeak from "./Feedbeak";
+import {
+  BrowserRouter as Router,
+  Route
+ } from "react-router-dom";
 
 function App() {
   return (
-
+    <Router>
+      <Header />
+      <Route>
+        <Feedbeak />
+      </Route>
+      <Route>
+        <ThanksReview />
+      </Route>
+    </Router>
   );
 }
 
