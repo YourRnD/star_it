0668e05cbb50820f55c9c04869caf27b33e278bc
VadimSido <vadsid74@gmail.com>
2021-03-25 21:07:07 +0200

    fix: Creating component Feedbeak and added style

---
+++ customer/src/Feedbeak/index.js
@@ -0,0 +1,64 @@
+import React, {useState, useEffect} from "react";
+import styles from "./Feedbeak.module.css";
+import FormFeedbeak from "./FormFeedbeak";
+import logo_silpo from "../../assets/logo_silpo.png";
+import logo_clock from "../../assets/logo_clock.png";
+
+const Feedbeak = () => {
+    const [date,setDate] = useState(new Date());
+
+    useEffect(() => {
+        const id = setInterval(() => setDate(new Date()), 1000);
+        return () => {
+            clearInterval(id);
+        }
+    }, []);
+
+    let serverResponse = {
+        'silpo' : {
+            businessName : 'Silpo',
+            address : 'Kiyv',
+            imgLogo : {logo_silpo}
+        },
+        '334455' : {
+            type : 'room',
+            nameType : 'room #100'
+        }
+    }
+
+    return(
+    <div>
+        <div className={styles.containerInfo}>
+            <div className={styles.containerPoint}>
+                <div className={styles.point}>
+                    <p>Name : <span className={styles.pointText}>{serverResponse.silpo.businessName}</span></p>
+                    <p>Adress : <span className={styles.pointText}>{serverResponse.silpo.address}</span></p>
+                    <p>Ticket window : <span className={styles.pointText}>{serverResponse[334455].nameType}</span></p>
+                </div>
+                <div className={styles.logo}>
+                    <img src={logo_silpo} className={styles.logoPoint} alt="LOGO" />
+                </div>
+            </div>
+            <div className={styles.clock}>
+                <img src={logo_clock} className={styles.clockLogo} alt="ICON" />
+                <p>Time : <span className={styles.clockText}>{date.toLocaleTimeString()} <small>({date.toLocaleDateString()})</small></span></p>
+            </div>
+        </div>
+        <div className={styles.containerRating}>
+            <h3>RATING</h3>
+            <ReactStars
+                classNames={styles.rating}
+                count={5}
+                value={5}
+                onChange={ratingChanged}
+                size={54}
+                activeColor="#ffd700"
+            /> 
+        </div>
+        <FormFeedbeak />    
+    </div>
+    );
+
+}
+
+export default Feedbeak;
