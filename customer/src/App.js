b0206633b86c90c1a34ac404301747099343bcfd
VadimSido <vadsid74@gmail.com>
2021-03-27 14:59:56 +0200

    fix: refactoring component App and feat Assets

--- customer/src/App.js
+++ customer/src/App.js
@@ -4,19 +4,15 @@ import Header from "./Header";
 import Feedbeak from "./Feedbeak";
 import {
   BrowserRouter as Router,
-  Route
+
  } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Header />
-      <Route>
-        <Feedbeak />
-      </Route>
-      <Route>
-        <ThanksReview />
-      </Route>
+      <Feedbeak />
+  
     </Router>
   );
 }
