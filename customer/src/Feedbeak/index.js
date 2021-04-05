fb85ec9cd6192bcd53589674357fe8655efb0105
VadimSido <vadsid74@gmail.com>
2021-04-05 21:58:34 +0300

    fix: Refactoring Feedbeak added PROPS

--- customer/src/Feedbeak/index.js
+++ customer/src/Feedbeak/index.js
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from "react";
 import styles from "./Feedbeak.module.css";
-import FormFeedbeak from "./FormFeedbeak";
-import ReactStars from "react-rating-stars-component";
-import logo_silpo from "../assets/logo_silpo.png";
 import logo_clock from "../assets/logo_clock.png";
+import ReactStars from "react-rating-stars-component";
+import FormFeedbeak from "./FormFeedbak/FormFeedbeak";
 
 
-const Feedbeak = () => {
+
+const Feedbeak = ({onChange, logo_bisnesses}) => {
     const [date,setDate] = useState(new Date());
 
     useEffect(() => {
@@ -20,50 +20,47 @@ const Feedbeak = () => {
         'silpo' : {
             businessName : 'Silpo',
             address : 'Kiyv',
-            imgLogo : {logo_silpo}
+            imgLogo : {logo_bisnesses}
         },
         '334455' : {
             type : 'room',
             nameType : 'room #100'
         }
     }
-    const [countStart,setCountStar] = useState(5);    
-    const ratingChanged = (newRating) => {
-      setCountStar(newRating);
-    };
-    return(
-    <div>
-        <div className={styles.containerInfo}>
-            <div className={styles.containerPoint}>
-                <div className={styles.point}>
-                    <p>Name : <span className={styles.pointText}>{serverResponse.silpo.businessName}</span></p>
-                    <p>Adress : <span className={styles.pointText}>{serverResponse.silpo.address}</span></p>
-                    <p>Ticket window : <span className={styles.pointText}>{serverResponse[334455].nameType}</span></p>
-                </div>
-                <div className={styles.logo}>
-                    <img src={logo_silpo} className={styles.logoPoint} alt="LOGO" />
-                </div>
-            </div>
-            <div className={styles.clock}>
-                <img src={logo_clock} className={styles.clockLogo} alt="ICON" />
-                <p>Time : <span className={styles.clockText}>{date.toLocaleTimeString()} <small>({date.toLocaleDateString()})</small></span></p>
-            </div>
-        </div>
-        <div className={styles.containerRating}>
-            <h3>RATING</h3>
-            <ReactStars
-                classNames={styles.rating}
-                count={5}
-                value={5}
-                onChange={ratingChanged}
-                size={54}
-                activeColor="#ffd700"
-            /> 
-        </div>
-        <FormFeedbeak />    
-    </div>
-    );
 
+    return(
+        <div className={styles.container}>
+            <div className={styles.containerInfo}>
+                <div className={styles.containerPoint}>
+                    <div className={styles.point}>
+                        <p>Name : <span className={styles.pointText}>{serverResponse.silpo.businessName}</span></p>
+                        <p>Adress : <span className={styles.pointText}>{serverResponse.silpo.address}</span></p>
+                        <p>Ticket window : <span className={styles.pointText}>{serverResponse[334455].nameType}</span></p>
+                    </div>
+                    <div className={styles.logo}>
+                        <img src={logo_bisnesses} className={styles.logoPoint} alt="LOGO" />
+                    </div>
+                </div>
+                <div className={styles.clock}>
+                    <img src={logo_clock} className={styles.clockLogo} alt="ICON" />
+                    <p>Time : <span className={styles.clockText}>{date.toLocaleTimeString()} <small>({date.toLocaleDateString()})</small></span></p>
+                </div>
+            </div>
+            <div className={styles.containerRating}>
+                <h3>RATING</h3>
+                <ReactStars
+                    classNames={styles.rating}
+                    count={5}
+                    value={5}
+                    onChange={onChange}
+                    size={54}
+                    activeColor="#ffd700"
+                />
+            </div>
+            <FormFeedbeak />
+        </div>
+    )
 }
+
 
 export default Feedbeak;
