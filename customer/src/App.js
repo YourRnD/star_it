870c16a00707ec11319cfb707c10110b9a5773e6
millisabel <38396309+millisabel@users.noreply.github.com>
2021-04-20 16:07:21 +0300

    feat:  App component

--- customer/src/App.js
+++ customer/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Scanner from "./components/Scanner/Scanner";
+import Nav from "./components/Nav/Nav";
+import Profile from "./components/Profile/Profile";
+import Statics from "./components/Statics/Statics";
+
+import './assets/style/layout/layout.css';
+import scanner from "./components/Scanner/Scanner.module.css";
+import TestLinks from "./components/TestLinks/TestLinks";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className={'wrap'}>
+                <Header/>
+                <Nav/>
+                <Route exact path="/">
+                    <p className={scanner.text}>Временный блок, эмуляция сканера</p>
+                    <TestLinks/>
+                </Route>
+                <Switch>
+                    <Route path="/statics" component={Profile} />
+                    <Route path="/info" component={Scanner} />
+                    <Route path="/profile" component={Statics} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
+
+
