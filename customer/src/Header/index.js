e21685194cf01a8916d66527fa5002a028156611
VadimSido <vadsid74@gmail.com>
2021-04-21 19:45:43 +0300

    feat: Added component QRcodePoint

--- customer/src/Header/index.js
+++ customer/src/Header/index.js
@@ -9,7 +9,7 @@ const Header = () => {
         <div className={styles.container}>
             <p><Link to="/statistics" className={styles.link}><strong>STATISTICS</strong></Link> </p>
             <p><Link to="/QR-code" className={`${styles.link} ${styles.active}`}><strong>QR-CODE</strong></Link> </p>
-            <p><Link to="profile" className={styles.link}><strong>PROFILE</strong></Link> </p>
+            <p><Link to="/profile" className={styles.link}><strong>PROFILE</strong></Link> </p>
         </div>
     )
 }
---
+++ customer/src/QRcodePoint/index.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+   } from "react-router-dom";
+import Feedbeak from '../Feedbeak';
+import { useParams } from "react-router-dom";
+import ThanksForReview from "../ThanksForReview";
+import FormFeedbeak from '../FormFeedbak';
+import logo_silpo from "../assets/logo_silpo.png";
+ 
+
+const QRcodePoint = () => {
+    const {idBisnesses} = useParams();
+    const {idPoint} = useParams();
+
+    const [countStar,setCountStar] = useState(5);    
+    const ratingChanged = (newRating) => {
+        setCountStar(newRating);
+      };
+
+      const [textFeedbeak, setTextFeedbeak] = useState('');
+      const onChangeFeedbeak = (e) => {
+          setTextFeedbeak(e.target.value);
+      };
+    
+    const urlBisnesses = 'www.star.it/api/business/' + idBisnesses; 
+    const urlPoint = 'www.star.it/api/fbo/' + idBisnesses + '/' + idPoint; 
+
+    const [error, setError] = useState(null);
+{/*    const [isLoaded, setIsLoaded] = useState(false); */}
+    const [isLoaded, setIsLoaded] = useState(true);
+    const [bisnesses, setBisnesses] = useState({});
+
+{/*    useEffect(() => {
+        fetch({urlBisnesses})
+            .then(rez => rez.json())
+            .then(
+                (rezult) => {
+                    setIsLoaded(true);
+                    setBisnesses(rezult);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    },[])  */};
+
+    let feedbeakToJson = {
+        rate : countStar,
+        date : new Date().toLocaleDateString(),
+        text : textFeedbeak
+    };
+    let jsonFeedbeak = JSON.stringify(feedbeakToJson); 
+    console.log(jsonFeedbeak);
+   
+    
+    if (error) {
+        return <div>Error: {error.message}</div>
+    }
+        else if (!isLoaded) {
+            return <div>Loading...</div>
+        }
+            else {
+                return (
+                    <Router>
+                        <Switch>
+                            <Route path="/QR-code/silpo/334455">
+                                <Feedbeak onChange={ratingChanged} logo_bisnesses={logo_silpo} />
+                                <FormFeedbeak value={textFeedbeak} onChange={onChangeFeedbeak} />
+                            </Route>
+                            <Route path="/report">
+                                <ThanksForReview countStar={countStar} logo_bisnesses={logo_silpo} feedbeakToJson={feedbeakToJson} />
+                            </Route> 
+                        </Switch> 
+                    </Router>  
+
+                );
+        }
+}
+
+export default QRcodePoint;
