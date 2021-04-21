d78bbd0bdc632301ee4cf1f16d70118d50a7a23b
VadimSido <vadsid74@gmail.com>
2021-04-21 19:53:20 +0300

    fix: Refactoring component and style FormFeedbeak

--- customer/src/FormFeedbeak/index.js
+++ customer/src/FormFeedbeak/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "./FormFeedbeak.module.css";
-import logo_camera from "../../assets/icon_camera.svg";
+import logo_camera from "../assets/icon_camera.svg";
 import {Link} from "react-router-dom";
 
-const FormFeedbeak = () => {
+const FormFeedbeak = ({value, onChange}) => {
     return(
         <div className={styles.container}>
             <form className={styles.containerForm}>
@@ -14,7 +14,7 @@ const FormFeedbeak = () => {
                 <button className={styles.imgCamera}>
                   <img src={logo_camera} alt="CAMERA" />
                 </button>
-                <textarea name="feedbeak" className={styles.feedbeakText} placeholder="feedbeak"></textarea>
+                <textarea name="feedbeak" className={styles.feedbeakText} value={value} onChange={onChange} placeholder="feedbeak"></textarea>
                 <button className={styles.sendButton}><Link to="/report"><strong>SEND</strong></Link></button>
             </form>
         </div>
