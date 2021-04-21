cdf730624992e9e234afef98cc4ca1866a31d10e
VadimSido <vadsid74@gmail.com>
2021-04-21 19:48:03 +0300

    fix: Refactoring component App

--- customer/src/App.js
+++ customer/src/App.js
@@ -1,27 +1,20 @@
-import React, {useState} from "react";
+import React from "react";
 import './App.css';
 import {
   BrowserRouter as Router,
   Route
  } from "react-router-dom";
 import Header from "./Header";
-import Feedbeak from "./Feedbeak";
-import ThanksForReview from "./ThanksForReview";
-import logo_silpo from "./assets/logo_silpo.png";
+import QRcodePoint from "./QRcodePoint";
 
 function App() {
-  const [countStar,setCountStar] = useState(5);    
-  const ratingChanged = (newRating) => {
-    setCountStar(newRating);
-  };
+
+    
   return (
     <Router className="App">
       <Header />
-      <Route path="/QR-code">
-        <Feedbeak onChange={ratingChanged} logo_bisnesses={logo_silpo} />
-      </Route>
-      <Route path="/report">
-        <ThanksForReview countStar={countStar} logo_bisnesses={logo_silpo} />
+      <Route path="/QR-code/:idBisnesses/:idPoint">
+        <QRcodePoint />
       </Route>
     </Router>
   );
